231ade216770bfb22c6163d26ef1d94d632b6482
Nivashini R <166809068+Nivashini-30@users.noreply.github.com>
2026-07-23 18:09:28 +0530

    Add files via upload

---
+++ Week7/Hands-on2/Task1/course-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './course-list.component.html'
+})
+export class CourseListComponent {}

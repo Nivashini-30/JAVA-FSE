0be9a720575d1b4deb42783c231f0baa6b99891a
Nivashini R <166809068+Nivashini-30@users.noreply.github.com>
2026-07-23 18:12:16 +0530

    Delete Week7/Hands-on2/course-list.component.ts

--- Week7/Hands-on2/course-list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-course-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './course-list.component.html'
-})
-export class CourseListComponent {}

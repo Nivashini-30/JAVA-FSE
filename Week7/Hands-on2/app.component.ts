0aabcaba12e2e5050f1c5931b6bd1fead17523bf
Nivashini R <166809068+Nivashini-30@users.noreply.github.com>
2026-07-23 17:57:26 +0530

    Add files via upload

---
+++ Week7/Hands-on2/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from '../pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'student-course-portal';
+}
